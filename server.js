80d44b8eea61525765889186109dff4a98c1bc82
Luis Huertas <hu3rtas@gmail.com>
2019-10-20 04:14:33 -0500

    Pequeña correccion

--- server.js
+++ server.js
@@ -1,34 +1,34 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var usuario=[ {
-    cod_trab:1,
-    nom_trab:"alicia",
-    ape_trab:"grimaldina",
-    cargo:"practicante",
-    nom_area:"uti",
-    dni_trab:"71268147",
+var usuario = [{
+    cod_trab: 1,
+    nom_trab: "alicia",
+    ape_trab: "grimaldina",
+    cargo: "practicante",
+    nom_area: "uti",
+    dni_trab: "71268147",
     usr_trab: "",
     psw_trab: "",
     fir_trab: "",
     fot_trab: "",
     est_trab: "",
     jefe_trab: "",
-    tije_trab:"",
-    acc_sir:"",
-    tipo_usuario:"practicante",
-    tipo_solicitud:"creacion de usuario",
-    motivo_solicitud:"personal nuevo",
-    servicios_red:" ",
-    acceso_red:[" "," "," "],
-    acceso_sistemas_administrativos:" ",
-    acceso_sistemas_registrales:"",
-    carpetas_compartidas:[" "," "," "," "],
-    jefe:["diego alquizar","69","jefe"],
-    sustento:" ",
-    observaciones:" ",
-    estado:"Creado"
-  }];
+    tije_trab: "",
+    acc_sir: "",
+    tipo_usuario: "practicante",
+    tipo_solicitud: "creacion de usuario",
+    motivo_solicitud: "personal nuevo",
+    servicios_red: " ",
+    acceso_red: [" ", " ", " "],
+    acceso_sistemas_administrativos: " ",
+    acceso_sistemas_registrales: "",
+    carpetas_compartidas: [" ", " ", " ", " "],
+    jefe: ["diego alquizar", "69", "jefe"],
+    sustento: " ",
+    observaciones: " ",
+    estado: "Creado"
+}];
 
 app.use(bodyParser.json());
 app.use(function (req, res, next) {
@@ -37,12 +37,12 @@ app.use(function (req, res, next) {
     next();
 });
 
-var altas_bajas=express.Router();
-altas_bajas.get('/', (req, res) =>{
+var altas_bajas = express.Router();
+altas_bajas.get('/', (req, res) => {
     res.json(usuario);
 });
 
-app.use("/FormularioAccesos",altas_bajas);
+app.use('/FormularioAccesos', altas_bajas);
 
 app.listen(7651);
 
